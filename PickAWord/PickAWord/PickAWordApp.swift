caf80d0afc73bdf89c9e6ce8d0e9b420bfdf2428
Detchat Boonpragob <oudy1st@gmail.com>
2022-07-02 10:39:05 +0700

    Initial xcode project
    create a new project and sort some file structure.

---
+++ PickAWord/PickAWord/PickAWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickAWordApp.swift
+//  PickAWord
+//
+//  Created by Detchat Boonpragob on 2/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PickAWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickAWord/PickAWord/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PickAWord
+//
+//  Created by Detchat Boonpragob on 2/7/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Please, pick some word to say!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
